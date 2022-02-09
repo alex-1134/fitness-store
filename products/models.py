0043e3ceb5809414df208f8b931b710bacd0cb9f
alex-1134 <alexa.t.lach@gmail.com>
2022-02-09 21:07:22 +0000

    Fix all urls

--- products/models.py
+++ products/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
-    
+
     class Meta:
         verbose_name_plural = 'Categories'
         
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254,  null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,11 +21,10 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    rating = models.DecimalField(max_digits=6, decimal_places=2,null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
